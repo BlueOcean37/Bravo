7a87daaabd64e404d82d2320918adb08295ac366
lerisse <tristanlerisse@gmail.com>
2021-07-12 19:09:38 -0400

    Fix issues on pull

--- server/db/index.js
+++ server/db/index.js
@@ -4,15 +4,15 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.PG_USER || 'postgres',
-  host: process.env.PG_HOST || 'localhost',
-  database: process.env.PG_DATABASE || 'test',
-  password: process.env.PG_PASSWORD || '',
-  port: process.env.PG_PORT || 5432,
+  user: process.env.PGUSER || 'postgres',
+  host: process.env.PGHOST || 'localhost',
+  database: process.env.PGDATABASE || 'blueocean',
+  password: process.env.PGPASSWORD || '',
+  port: process.env.PGPORT || 5432,
 });
 
 pool.query('SELECT NOW()', (err, res) => {
-  console.log('Connected to Postgres');
+  console.log(err, res);
   // pool.end();
 });
 
