ec162e5f8dc154f63dfb932b10e98df91d98b263
Warren Wong <warren.wong4@gmail.com>
2021-07-12 13:24:22 -0400

    Implement addUser controller

--- server/controllers/users/addUser.js
+++ server/controllers/users/addUser.js
@@ -1,7 +1,13 @@
-// POST add a USER controller
+const pool = require('../../db/index');
 
 const addUser = (req, res) => {
-  res.send('hello world');
+  const { username, password, email, first_name, last_name, photo } = req.body;
+  let queryString = `INSERT INTO users ( username, password, email, first_name, last_name, photo) VALUES ('${username}', '${password}', '${email}', '${first_name}', '${last_name}', '${photo}')`;
+
+  pool
+    .query(queryString)
+    .then(() => res.sendStatus(201))
+    .catch((err) => res.status(400).json(err));
 };
 
 module.exports = addUser;
