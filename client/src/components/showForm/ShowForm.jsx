2ec0ce8b124ae8404f895b7e6c449d8f9f33aaa7
timapar <timothy_parrish@hotmail.com>
2021-07-13 17:45:19 -0400

    implement basic functionality
    STILL TO BE IMPLEMENTED
    -user_id is harcoded, need a value somehow
    -fix date to be a start date and end date
    -implement onSubmit pop-ups
    -figure out picture file uploading

--- client/src/components/showForm/ShowForm.jsx
+++ client/src/components/showForm/ShowForm.jsx
@@ -1,5 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button } from '@material-ui/core';
+import axios from 'axios';
+import styles from './showForm.module';
 
 export default function ShowForm() {
-  return <div>Show Form Main Page</div>;
+  const [title, setTitle] = useState('');
+  const [street, setStreet] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [zip, setZip] = useState('');
+  const [date, setDate] = useState('');
+  const [website, setWebsite] = useState('');
+  const [cast, setCast] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formValidation()) {
+      const epochDate = Date.parse(date);
+      const newShow = {
+        user_id: 1,
+        title,
+        street,
+        city,
+        state,
+        zip,
+        date: epochDate,
+        website,
+        cast,
+        description,
+      };
+      axios
+        .post('/shows/', newShow)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    } else {
+      alert('Please enter a value for all required fields');
+    }
+  };
+
+  const formValidation = () => {
+    return title && street && city && state && zip && date;
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>ADD YOUR SHOW!</h1>
+      <div className={styles.form}>
+        <div className={styles.input}>
+          <TextField
+            id="title"
+            label="Show Title"
+            required
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <TextField
+            id="street"
+            label="Street Address"
+            required
+            onChange={(e) => setStreet(e.target.value)}
+          />
+          <TextField id="city" label="City" required onChange={(e) => setCity(e.target.value)} />
+          <TextField id="state" label="State" required onChange={(e) => setState(e.target.value)} />
+          <TextField id="zip" label="Zipcode" required onChange={(e) => setZip(e.target.value)} />
+          <TextField
+            id="date"
+            label="Dates"
+            required
+            type="date"
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <TextField id="website" label="Website" onChange={(e) => setWebsite(e.target.value)} />
+          <TextField
+            id="cast"
+            label="Cast/Crew"
+            multiline
+            rows={5}
+            onChange={(e) => setCast(e.target.value)}
+          />
+          <TextField
+            id="description"
+            label="Description"
+            multiline
+            rows={5}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className={styles.photoContainer}>
+          <Button className={styles.btn}>Add Photo</Button>
+          <Button className={styles.btn} onClick={handleSubmit}>
+            Submit
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
