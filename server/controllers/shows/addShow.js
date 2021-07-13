67bcafa942a21789d6f0610c05de39cebd07e90c
timapar <timothy_parrish@hotmail.com>
2021-07-12 22:46:59 -0400

    I shouldn't have changed date to timestamp, it is the show date

--- server/controllers/shows/addShow.js
+++ server/controllers/shows/addShow.js
@@ -2,16 +2,15 @@
 const pool = require('../../db/index');
 
 const addShow = (req, res) => {
-  const { title, street, city, zip, state, cast, website, description, photo, user_id } = req.body;
-
-  const timestamp = new Date().getTime();
+  const { title, street, city, zip, state, cast, website, description, photo, user_id, date } =
+    req.body;
 
   const queryString = `
   INSERT INTO shows
   (user_id, title, street, city, zip, state, date, "cast", website, description, photo)
   VALUES (
   '${user_id}', '${title}', '${street}', '${city}',
-  '${zip}','${state}', '${timestamp}', '${cast}', '${website}',
+  '${zip}','${state}', '${date}', '${cast}', '${website}',
   '${description}', '${photo}'
   )`;
 
