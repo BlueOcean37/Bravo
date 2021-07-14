e6bb1c1dd67e692eec6ba526b6e211ba06502412
timapar <timothy_parrish@hotmail.com>
2021-07-13 22:24:58 -0400

    implement submission dialog pop up

--- client/src/components/showForm/ShowForm.jsx
+++ client/src/components/showForm/ShowForm.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
-import { TextField, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import {
+  TextField,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@material-ui/core';
 import { DropzoneArea } from 'material-ui-dropzone';
 import axios from 'axios';
 import styles from './showForm.module';
@@ -43,12 +52,28 @@ export default function ShowForm() {
         description,
       };
       axios
-        .post('/shows/', newShow)
-        .then((res) => console.log(res))
-        .catch((err) => console.log(err));
+        .post('/api/shows/', newShow)
+        .then((res) => setSubmitDialogOpen(true))
+        .catch((err) => {
+          console.error('There was an error', err);
+          alert('There was a problem submitting your show, please try again');
+        });
     } else {
       alert('Please enter a value for all required fields');
     }
+  };
+
+  const resetForm = () => {
+    setTitle('');
+    setStreet('');
+    setCity('');
+    setState('');
+    setZip('');
+    setDate('');
+    setWebsite('');
+    setCast('');
+    setDescription('');
+    setPhoto('');
   };
 
   return (
@@ -59,21 +84,42 @@ export default function ShowForm() {
           <TextField
             id="title"
             label="Show Title"
+            value={title}
             required
             onChange={(e) => setTitle(e.target.value)}
           />
           <TextField
             id="street"
             label="Street Address"
+            value={street}
             required
             onChange={(e) => setStreet(e.target.value)}
           />
-          <TextField id="city" label="City" required onChange={(e) => setCity(e.target.value)} />
-          <TextField id="state" label="State" required onChange={(e) => setState(e.target.value)} />
-          <TextField id="zip" label="Zipcode" required onChange={(e) => setZip(e.target.value)} />
+          <TextField
+            id="city"
+            label="City"
+            value={city}
+            required
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <TextField
+            id="state"
+            label="State"
+            value={state}
+            required
+            onChange={(e) => setState(e.target.value)}
+          />
+          <TextField
+            id="zip"
+            label="Zipcode"
+            value={zip}
+            required
+            onChange={(e) => setZip(e.target.value)}
+          />
           <TextField
             id="date"
             label="Dates"
+            value={date}
             required
             type="date"
             onChange={(e) => setDate(e.target.value)}
@@ -82,6 +128,7 @@ export default function ShowForm() {
           <TextField
             id="cast"
             label="Cast/Crew"
+            value={cast}
             multiline
             rows={5}
             onChange={(e) => setCast(e.target.value)}
@@ -89,6 +136,7 @@ export default function ShowForm() {
           <TextField
             id="description"
             label="Description"
+            value={description}
             multiline
             rows={5}
             onChange={(e) => setDescription(e.target.value)}
@@ -105,9 +153,37 @@ export default function ShowForm() {
               }
             }}
           />
-          <Button className={styles.btn} onClick={handleSubmit}>
-            Submit
-          </Button>
+          <div>
+            <Button className={styles.btn} onClick={handleSubmit}>
+              Submit
+            </Button>
+            <Dialog
+              open={submitDialogOpen}
+              aria-labelledby="submit-dialog-title"
+              aria-describedby="submit-dialog-description"
+            >
+              <DialogTitle id="submit-dialog-title">{'Your show has been submitted'}</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="submit-dialog-description">
+                  Your show has successfully been submitted. Would you like to add another show or
+                  return to the homepage?
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button
+                  onClick={() => {
+                    resetForm();
+                    setSubmitDialogOpen(false);
+                  }}
+                >
+                  <Link to="/addShow">Add another show</Link>
+                </Button>
+                <Button onClick={() => setSubmitDialogOpen(false)} autoFocus>
+                  <Link to="/">Go to the homepage</Link>
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </div>
         </div>
       </div>
     </div>
