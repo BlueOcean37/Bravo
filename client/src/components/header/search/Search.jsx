0339a4e47ea496641f119b81ecdeb61efefd2c30
lerisse <tristanlerisse@gmail.com>
2021-07-15 11:57:22 -0400

    Styled search results

--- client/src/components/header/search/Search.jsx
+++ client/src/components/header/search/Search.jsx
@@ -9,17 +9,50 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import { createFilterOptions } from '@material-ui/lab/Autocomplete';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Rating from '@material-ui/lab/Rating';
+
 function sleep(delay = 0) {
   return new Promise((resolve) => {
     setTimeout(resolve, delay);
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    border: '0px solid #e0e0e0',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+}));
+
 export default function Search() {
   const [open, setOpen] = React.useState(false);
   const [showId, setShowid] = React.useState();
   const [options, setOptions] = React.useState([]);
   const loading = open && options.length === 0;
+
+  const classes = useStyles();
+  const theme = useTheme();
 
   React.useEffect(() => {
     let active = true;
@@ -56,7 +89,7 @@ export default function Search() {
   return (
     <Autocomplete
       id="search"
-      style={{ width: 300, background: 'white' }}
+      style={{ width: 500, background: 'white' }}
       open={open}
       onOpen={() => {
         setOpen(true);
@@ -71,15 +104,25 @@ export default function Search() {
       filterOptions={filterOptions}
       renderOption={(option) => (
         <>
-          <a
-            onClick={() => {
-              setShowid(option.id);
-              <Link to={`'/shows/${option.id}'`}> Home </Link>;
-            }}
-          >
-            <img className={searchStyle.photo} alt="show image" src={option.photo}></img>
-            <p>{option.title}</p>
-          </a>
+          <Link to={`/shows/${option.id}`}>
+            <Card className={classes.root}>
+              <CardMedia className={classes.cover} image={`${option.photo}`} title={option.title} />
+              <div className={classes.details}>
+                <CardContent className={classes.content}>
+                  <Typography component="h5" variant="body1">
+                    {option.title}
+                  </Typography>
+                  <Rating
+                    name="half-rating-read"
+                    value={option.rating}
+                    precision={0.1}
+                    size="small"
+                    readOnly
+                  />
+                </CardContent>
+              </div>
+            </Card>
+          </Link>
         </>
       )}
       renderInput={(params) => (
