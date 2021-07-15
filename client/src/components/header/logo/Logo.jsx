6406513d75469977d58f79aba5a9fab8e92beab6
lerisse <tristanlerisse@gmail.com>
2021-07-14 20:44:35 -0400

    Added Logo onClick event + history forward to homepage

--- client/src/components/header/logo/Logo.jsx
+++ client/src/components/header/logo/Logo.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import logoStyles from './logo.module';
+import { useHistory } from 'react-router-dom';
+
+import { Link } from 'react-router-dom';
 
 export default function Logo() {
-  const logo =
-    'https://raw.githubusercontent.com/BlueOcean37/Bravo/dev-environment-test/client/src/assets/applogo.png';
+  const history = useHistory();
+  const handleOnClick = useCallback(() => history.push('/'), [history]);
 
-  return <img className={logoStyles.logo} src={`${logo}`} alt="Teatro Logo" />;
+  const logo =
+    'https://raw.githubusercontent.com/BlueOcean37/Bravo/header-component/client/src/assets/applogo-removebg-preview.png';
+
+  return (
+    <img className={logoStyles.logo} src={`${logo}`} alt="Teatro Logo" onClick={handleOnClick} />
+  );
 }
