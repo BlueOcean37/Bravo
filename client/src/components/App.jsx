650fd618f5dcd4e61f4974084613d3d30b0ce16a
Diego Coronel <diegocoronel1997@gmail.com>
2021-07-15 21:43:51 -0400

    layout

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,11 +9,15 @@ import ShowForm from './showForm/ShowForm';
 import Signup from './authentication/signup/Signup';
 import NavBar from './header/navbar/NavBar';
 import NotFound from './notFound/NotFound';
+import styles from './app.module.scss';
 
 export default function App() {
   return (
     <Router>
       <div id="mainContainer">
+        <div id={styles.test}>
+          <div id={styles.son} />
+        </div>
         <header id="mainHeader">
           <Header />
         </header>
