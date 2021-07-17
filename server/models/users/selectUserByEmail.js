5340acd6efd7f60926a19e7cfd250256d4edf7ee
Warren Wong <warren.wong4@gmail.com>
2021-07-17 12:38:18 -0400

    Update get user by email MVC

---
+++ server/models/users/selectUserByEmail.js
@@ -0,0 +1,11 @@
+const pool = require('../../db/index');
+
+const selectUserByEmail = (data) => {
+  let queryString = `SELECT id
+  FROM users
+  WHERE email = '${data}';`;
+
+  return pool.query(queryString);
+};
+
+module.exports = selectUserByEmail;
