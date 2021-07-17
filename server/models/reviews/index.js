68613a4a372baaea3849b1441e12b40db6fb23df
Warren Wong <warren.wong4@gmail.com>
2021-07-17 11:55:56 -0400

    Update get comments MVC

--- server/models/reviews/index.js
+++ server/models/reviews/index.js
@@ -2,10 +2,12 @@ const insertComment = require('./insertComment');
 const insertReview = require('./insertReview');
 const updateReviewDownVote = require('./updateReviewDownVote');
 const selectAllReviews = require('./selectAllReviews');
+const selectComments = require('./selectComments');
 
 module.exports = {
   insertComment,
   insertReview,
   updateReviewDownVote,
   selectAllReviews,
+  selectComments,
 };
